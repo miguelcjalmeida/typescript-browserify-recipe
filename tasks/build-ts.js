fbdeb5d0b11b912c0d70877df969f0346a0a3bac
Miguel Almeida <darkmagusbr@gmail.com>
2016-10-22 23:34:53 -0200

    add sourcemaps sourceRoot relative up to src/ directory

--- tasks/build-ts.js
+++ tasks/build-ts.js
@@ -20,7 +20,7 @@ gulp.task('build-ts', [], () => {
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify())
     .pipe(rename({extname: '.min.js'}))
-    .pipe(sourcemaps.write('./'))
+    .pipe(sourcemaps.write('./', {sourceRoot: '../'}))
     .pipe(gulp.dest('./dist'))
 })
 
